f508d67882656ba8cc5a7a45f4958f62f53a0e9b
Pablo <pablo.perez@techedgegroup.com>
2022-07-26 12:12:01 +0200

    Inicializado proyecto con Spring Initializr

---
+++ src/main/java/ejercicio/ampliaiot/microstreaminganalytics/MicrostreamingAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package ejercicio.ampliaiot.microstreaminganalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrostreamingAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrostreamingAnalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ejercicio/ampliaiot/microstreaminganalytics/MicrostreamingAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package ejercicio.ampliaiot.microstreaminganalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrostreamingAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
