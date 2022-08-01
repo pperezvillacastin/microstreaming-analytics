bdc954e497eaffd2e7bd68894a80d15eae68a567
Pablo <pablo.perez@techedgegroup.com>
2022-08-01 09:32:47 +0200

    Separar la extracción y el análisis de datos en distintas clases

--- src/main/java/ejercicio/ampliaiot/microstreaminganalytics/ScheduledRabbitMQReceiver.java
+++ src/main/java/ejercicio/ampliaiot/microstreaminganalytics/ScheduledRabbitMQReceiver.java
@@ -26,8 +26,6 @@ import java.util.stream.Collectors;
 @Component
 @Log
 public class ScheduledRabbitMQReceiver {
-    @Autowired
-    StatisticalDataRepository statisticalDataRepository;
 
     @Value("${spring.rabbitmq.host}")
     private String host ;
@@ -40,9 +38,12 @@ public class ScheduledRabbitMQReceiver {
     @Value("${rabbitmq.consumer.queuename}")
     private String queue ;
 
+    @Autowired
+    StatisticalDataWriter statisticalDataWriter;
+
+
     //Esto no deberia estar hardcodeado
     private static final String dataToExtract = "flowrate";
-
 
     @Scheduled(fixedRateString = "${queue.read.rate}" , initialDelay = 20000)
     public void readQueue() throws IOException, TimeoutException {
@@ -57,44 +58,25 @@ public class ScheduledRabbitMQReceiver {
             Channel channel = connection.createChannel();
             AMQP.Queue.DeclareOk declareOk = channel.queueDeclarePassive(queue);
 
-            DescriptiveStatistics descriptiveStatistics = new DescriptiveStatistics();
             List<Double> dataList = new LinkedList<Double>();
             GetResponse getResponse = null;
             do {
                 getResponse = channel.basicGet(queue, true);
                 if (getResponse == null) break;
                 String jsonBody = new String(getResponse.getBody());
-                descriptiveStatistics.addValue(extractDataStreamSingleValue(jsonBody,dataToExtract));
                 dataList.add(extractDataStreamSingleValue(jsonBody,dataToExtract));
             }
             while (getResponse != null);
             channel.close();
             connection.close();
+            if (dataList.size()==0) {
+                log.info("Empty data set. Aborting:");
+                return;
+            }
+            else {
+                statisticalDataWriter.dataListToStatisticalData(dataList,dataToExtract);
+            }
 
-
-
-            StatisticalData statisticalData = new StatisticalData(
-                    RandomStringUtils.randomAlphabetic(12), //id
-                    dataToExtract, //tipo de dato
-                    new Date(), // timestamp
-                    descriptiveStatistics.getMean(), //media
-                    descriptiveStatistics.getPercentile(50), //mediana
-                    getMode(dataList), // moda
-                    descriptiveStatistics.getStandardDeviation(), //desviacion estandar
-                    new ArrayList<Double>(Arrays.asList(
-                            descriptiveStatistics.getPercentile(25),
-                            descriptiveStatistics.getPercentile(50),
-                            descriptiveStatistics.getPercentile(75)
-                    )), //cuartiles
-                    descriptiveStatistics.getMax(), // max
-                    descriptiveStatistics.getMin() // min
-            );
-            StatisticalData result = statisticalDataRepository.insert(statisticalData);
-            String listString = dataList.stream().map(Object::toString).collect(Collectors.joining(", "));
-            log.info("data set:");
-            log.info(listString);
-            log.info("result set:");
-            log.info(result.toString());
         } catch (Exception e) {
             log.info(ExceptionUtils.getStackTrace(e));
         }
@@ -120,50 +102,14 @@ public class ScheduledRabbitMQReceiver {
         // que opere sobre un mensaje es bastante mas sencillo), pero el ejemplo
         // en el que me base (erroneamente) de la documentacion tenia un solo campo
         JsonNode datastream = datastreams.get(0);
+        if (datastream == null) {
+            throw new Exception("Null datastream received");
+        }
         if (!datastream.get("id").textValue().equals(datastreamtype)  ){
             throw new Exception("Wrong stream type received " + datastream.get("id").textValue() + datastreamtype + " expected");
         }
         //Igual que antes, solo considero un data point por fichero (pero multiples elementos en la cola)
         return datastream.get("datapoints").get(0).get("value").doubleValue();
     }
-
-    // He intentado calcula la moda con apache commons pero me devolvia la lista
-    // de entrada entera. Saco este otro ejemplo de Stack Overflow (sabria
-    // calcularla para integers pero no para doubles)
-    // https://stackoverflow.com/a/38937305
-    // Generalmente va a devolver todos los elementos de entrada a no ser que deliberadamente
-    // repitamos alguno
-    private static Set<Double> getMode(double[] data) {
-        if (data.length == 0) {
-            return new TreeSet<>();
-        }
-        TreeMap<Double, Integer> map = new TreeMap<>(); //Map Keys are array values and Map Values are how many times each key appears in the array
-        for (int index = 0; index != data.length; ++index) {
-            double value = data[index];
-            if (!map.containsKey(value)) {
-                map.put(value, 1); //first time, put one
-            }
-            else {
-                map.put(value, map.get(value) + 1); //seen it again increment count
-            }
-        }
-        Set<Double> modes = new TreeSet<>(); //result set of modes, min to max sorted
-        int maxCount = 1;
-        Iterator<Integer> modeApperance = map.values().iterator();
-        while (modeApperance.hasNext()) {
-            maxCount = Math.max(maxCount, modeApperance.next()); //go through all the value counts
-        }
-        for (double key : map.keySet()) {
-            if (map.get(key) == maxCount) { //if this key's value is max
-                modes.add(key); //get it
-            }
-        }
-        return modes;
-    }
-
-    private static Set<Double> getMode(List<Double> dataList){
-        return getMode(StatUtils.mode(dataList.stream().mapToDouble(Double::doubleValue).toArray()));
-    }
-
 
 }
